0ba0ef91c22709362cf3c35c8a5bb05f9745cf46
SidiMuhammad <60999516+SidiMuhammad@users.noreply.github.com>
2021-10-20 01:35:09 +0700

    first-commit

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 $(document).ready(function(){
+    responsive();
+    
     $(window).on('resize', function(){
+        responsive();
+    });
+
+    function responsive () {
         if (window.innerWidth < window.innerHeight) {
             $("svg").attr({"width":"69.33vw", "height":"55vw"});
         } else if (window.innerWidth > window.innerHeight) {
             $("svg").attr({"width":"69.33vh", "height":"55vh"});
         }
-    });
+    }
 });
